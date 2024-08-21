382c7ed12569ec6772df56af3c5ccdc08e0f8e09
Alberthzaid <alberthzaid2003@gmail.com>
2024-08-21 16:38:50 -0500

    upload api

--- src/Helpers/ApiConfig/Controllers/Apis.js
+++ src/Helpers/ApiConfig/Controllers/Apis.js
@@ -1,3 +1,3 @@
 export const API_GET_PRODUCTS = "https://fakestoreapi.com/products";
 
-export const API_PAYMENT_PRODUCT = ' http://localhost:3000/create_preference'
+export const API_PAYMENT_PRODUCT = ' https://backend-technicaltest.onrender.com/create_preference'
